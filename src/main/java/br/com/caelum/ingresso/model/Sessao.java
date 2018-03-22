985b54cba55eb774ebad62fbaae5ffe1f6aaca50
Wallef <wallef.fa@gmail.com>
2018-03-22 19:41:00 -0300

    Atualizando sistema

--- src/main/java/br/com/caelum/ingresso/model/Sessao.java
+++ src/main/java/br/com/caelum/ingresso/model/Sessao.java
@@ -3,19 +3,31 @@ package br.com.caelum.ingresso.model;
 import java.time.LocalTime;
 
 import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.ManyToOne;
 
 @Entity
 public class Sessao {
 	
+	@Id
+	@GeneratedValue
 	private Integer id;
 	private LocalTime horario;
+	@ManyToOne
 	private Filme filme;
+	
+	@ManyToOne
 	private Sala sala;
 	
 	public Sessao(LocalTime horario, Filme filme, Sala sala) {
 		this.horario = horario;
 		this.filme = filme;
 		this.sala = sala;
+	}
+	
+	public Sessao() {
+		
 	}
 	
 	public LocalTime getHorarioTermino() {
@@ -53,6 +65,5 @@ public class Sessao {
 	public void setSala(Sala sala) {
 		this.sala = sala;
 	}
-	
 	
 }
